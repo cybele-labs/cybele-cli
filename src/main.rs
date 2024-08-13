f5c7deeb60d44639d5ca85dceb90ec16a45188a2
t-bast <bastien@acinq.fr>
2024-08-13 20:13:17 +0200

    Detect typos in master password
    It's really annoying to type the master password twice, every time we
    want to create a new password or obtain an existing one. But it's quite
    dangerous to not be able to detect typos, which could prevent recovery
    of newly created passwords.
    
    To protect against this, we ask for the master password twice the first
    time it is entered. Once it has been entered successfully twice, we keep
    a randomized hash of it in memory. The next time we ask the user to
    enter the master password, we compare the result to the stored hash: if
    they match, we don't need to enter the password again.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,11 @@ extern crate rpassword;
 extern crate rprompt;
 
 use std::env;
+use std::io::Write;
 use std::path::Path;
+
+use rand::rngs::OsRng;
+use rand::RngCore;
 
 use cybele_core::vault::Vault;
 
@@ -29,16 +33,62 @@ fn parse_startup_args() -> Result<StartupArgs, ()> {
     Ok(StartupArgs { vault_file, vault_format })
 }
 
-fn prompt_master_password() -> String {
-    rpassword::prompt_password("Enter your master password: ").unwrap()
+/// The first time the master password is entered, we save a randomized hash of it for the duration
+/// of the process. This allows detecting errors when the password is later entered again without
+/// keeping the password itself in memory.
+struct MasterPasswordHash {
+    salt: [u8; 32],
+    hash: Option<[u8; 32]>,
 }
 
-fn prompt_password_repeat(prompt: &str) -> String {
-    let password = rpassword::prompt_password(prompt).unwrap();
+impl MasterPasswordHash {
+    pub fn new() -> Self {
+        // Initialize random salt.
+        let mut csprng = OsRng {};
+        let mut salt: [u8; 32] = [0u8; 32];
+        csprng.fill_bytes(&mut salt);
+        MasterPasswordHash { salt, hash: None }
+    }
+
+    pub fn compute_hash(&self, password: &str) -> [u8; 32] {
+        let mut msg: Vec<u8> = Vec::with_capacity(32 + password.len());
+        msg.write_all(&self.salt).unwrap();
+        msg.write_all(password.as_bytes()).unwrap();
+        cybele_core::hash::sha256::hash(&msg)
+    }
+}
+
+fn prompt_master_password(master_hash: &mut MasterPasswordHash) -> String {
+    let password = rpassword::prompt_password("Please enter your master password: ").unwrap();
+    let hash = master_hash.compute_hash(&password);
+    match master_hash.hash {
+        None => {
+            let password2 = rpassword::prompt_password("Please enter your master password again: ").unwrap();
+            if password != password2 {
+                println!("The two passwords are not equal, let's try again...");
+                prompt_master_password(master_hash)
+            } else {
+                master_hash.hash = Some(hash);
+                password
+            }
+        }
+        Some(expected_hash) => {
+            if hash != expected_hash {
+                println!("The password is different from the last master password, let's try again...");
+                prompt_master_password(master_hash)
+            } else {
+                password
+            }
+        }
+    }
+}
+
+fn create_password() -> String {
+    let password = rpassword::prompt_password("Password: ").unwrap();
     let password2 = rpassword::prompt_password("Please enter password again: ").unwrap();
     if password != password2 {
         println!("The two passwords are not equal, let's try again...");
-        prompt_password_repeat(prompt)
+        create_password()
     } else {
         password
     }
@@ -96,12 +146,13 @@ fn main() {
         Err(()) => return,
     };
 
+    let mut master_password_hash = MasterPasswordHash::new();
     let mut vault: Vault = if !Path::new(&args.vault_file).exists() {
         println!("Vault file does not exist: creating a new vault...");
         Vault::create(None)
     } else {
         println!("Loading encrypted vault...");
-        let password = prompt_master_password();
+        let password = prompt_master_password(&mut master_password_hash);
         let loading = match args.vault_format {
             VaultFormat::Plain => file::load_plain_vault(&args.vault_file, &password),
             VaultFormat::Image => file::load_image_vault(&args.vault_file, &password),
@@ -126,17 +177,17 @@ fn main() {
                 let name = rprompt::prompt_reply("Name: ").unwrap();
                 let hidden = rprompt::prompt_reply("Hide input (y/n): ").unwrap();
                 let password = match hidden.as_str() {
-                    "y" => prompt_password_repeat("Password: "),
+                    "y" => create_password(),
                     _ => rprompt::prompt_reply("Password: ").unwrap(),
                 };
-                let master_password = prompt_master_password();
+                let master_password = prompt_master_password(&mut master_password_hash);
                 add_to_vault(&mut vault, &name, &password, &master_password);
             }
             "generate" => {
                 let name = rprompt::prompt_reply("Name: ").unwrap();
                 let password_len: usize = rprompt::prompt_reply("Password length: ").unwrap().parse().unwrap();
                 let password = generate_password(password_len);
-                let master_password = prompt_master_password();
+                let master_password = prompt_master_password(&mut master_password_hash);
                 add_to_vault(&mut vault, &name, &password, &master_password);
             }
             "remove" => {
@@ -150,7 +201,7 @@ fn main() {
                 let id: usize = rprompt::prompt_reply("ID: ").unwrap().parse().unwrap();
                 if id < items.len() {
                     let name: &str = &items[id];
-                    let master_password = prompt_master_password();
+                    let master_password = prompt_master_password(&mut master_password_hash);
                     match vault.get(name, &master_password) {
                         Some(password) => {
                             println!("  - name: {}", name);
@@ -171,7 +222,7 @@ fn main() {
                 });
             }
             "save" => {
-                let master_password = prompt_master_password();
+                let master_password = prompt_master_password(&mut master_password_hash);
                 match args.vault_format {
                     VaultFormat::Plain => file::save_plain_vault(&vault, &args.vault_file, &master_password),
                     VaultFormat::Image => {
