4fd190fe048b7aa5d9419e3b229d19f297723de4
t-bast <bastien@acinq.fr>
2024-08-13 19:17:06 +0200

    Add option to generate passwords
    And some cosmetic and linter changes.

--- src/main.rs
+++ src/main.rs
@@ -29,14 +29,27 @@ fn parse_startup_args() -> Result<StartupArgs, ()> {
     Ok(StartupArgs { vault_file, vault_format })
 }
 
-fn prompt_password(prompt: &str) -> String {
+fn prompt_master_password() -> String {
+    rpassword::prompt_password("Enter your master password: ").unwrap()
+}
+
+fn prompt_password_repeat(prompt: &str) -> String {
     let password = rpassword::prompt_password(prompt).unwrap();
     let password2 = rpassword::prompt_password("Please enter password again: ").unwrap();
     if password != password2 {
         println!("The two passwords are not equal, let's try again...");
-        prompt_password(prompt)
+        prompt_password_repeat(prompt)
     } else {
         password
+    }
+}
+
+fn generate_password(password_len: usize) -> String {
+    let password = cybele_core::password::generate_password(password_len);
+    let regen = rprompt::prompt_reply(format!("Password = {}, do you want to generate a new one (y/n)? ", password)).unwrap();
+    match regen.as_str() {
+        "y" => generate_password(password_len),
+        _ => password,
     }
 }
 
@@ -52,9 +65,22 @@ fn prompt_color() -> file::Color {
     }
 }
 
+fn add_to_vault(vault: &mut Vault, name: &str, password: &str, master_password: &str) {
+    match vault.add(name, password, master_password) {
+        Some(_) => {
+            println!("Password added for <{}>", &name);
+            println!("Don't forget to use the `save` command to save your changes.");
+        }
+        None => {
+            println!("Password could not be added for <{}>", &name);
+        }
+    };
+}
+
 fn help() {
     println!("Available commands:");
     println!("  - add");
+    println!("  - generate");
     println!("  - remove");
     println!("  - get");
     println!("  - list");
@@ -75,7 +101,7 @@ fn main() {
         Vault::create(None)
     } else {
         println!("Loading encrypted vault...");
-        let password = prompt_password("Master password: ");
+        let password = prompt_master_password();
         let loading = match args.vault_format {
             VaultFormat::Plain => file::load_plain_vault(&args.vault_file, &password),
             VaultFormat::Image => file::load_image_vault(&args.vault_file, &password),
@@ -93,42 +119,42 @@ fn main() {
     println!();
     loop {
         let command = rprompt::prompt_reply("> ").unwrap();
+        println!();
         match command.as_str() {
             "help" => help(),
             "add" => {
                 let name = rprompt::prompt_reply("Name: ").unwrap();
                 let hidden = rprompt::prompt_reply("Hide input (y/n): ").unwrap();
                 let password = match hidden.as_str() {
-                    "y" => prompt_password("Password: "),
+                    "y" => prompt_password_repeat("Password: "),
                     _ => rprompt::prompt_reply("Password: ").unwrap(),
                 };
-                let master_password = prompt_password("Master password: ");
-                match vault.add(&name, &password, &master_password) {
-                    Some(_) => {
-                        println!("Password added for <{}>", &name);
-                        println!("Don't forget to use the `save` command to save your changes.");
-                    }
-                    None => {
-                        println!("Password could not be added for <{}>", &name);
-                    }
-                };
+                let master_password = prompt_master_password();
+                add_to_vault(&mut vault, &name, &password, &master_password);
+            }
+            "generate" => {
+                let name = rprompt::prompt_reply("Name: ").unwrap();
+                let password_len: usize = rprompt::prompt_reply("Password length: ").unwrap().parse().unwrap();
+                let password = generate_password(password_len);
+                let master_password = prompt_master_password();
+                add_to_vault(&mut vault, &name, &password, &master_password);
             }
             "remove" => {
                 let name = rprompt::prompt_reply("Name: ").unwrap();
                 vault.remove(name.as_str());
-                println!("Password for <{}> removed", name);
-                println!("Don't forget to use the `save` command to save your changes.");
+                println!("  Password for <{}> removed", name);
+                println!("  Don't forget to use the `save` command to save your changes.");
             }
             "get" => {
                 let items = vault.list();
                 let id: usize = rprompt::prompt_reply("ID: ").unwrap().parse().unwrap();
                 if id < items.len() {
                     let name: &str = &items[id];
-                    let master_password = prompt_password("Master password: ");
+                    let master_password = prompt_master_password();
                     match vault.get(name, &master_password) {
                         Some(password) => {
-                            println!("name: {}", name);
-                            println!("password: {}", &password);
+                            println!("  - name: {}", name);
+                            println!("  - password: {}", &password);
                         }
                         None => println!("Could not find or decrypt password for <{}>", name),
                     }
@@ -140,12 +166,12 @@ fn main() {
                 let filter = rprompt::prompt_reply("Filter: ").unwrap();
                 vault.list().iter().enumerate().for_each(|(pos, item)| {
                     if item.contains(&filter) {
-                        println!("{}: {}", pos, item)
+                        println!("  - {}: {}", pos, item)
                     }
                 });
             }
             "save" => {
-                let master_password = prompt_password("Master password: ");
+                let master_password = prompt_master_password();
                 match args.vault_format {
                     VaultFormat::Plain => file::save_plain_vault(&vault, &args.vault_file, &master_password),
                     VaultFormat::Image => {
@@ -157,5 +183,6 @@ fn main() {
             "exit" => break,
             _ => println!("Unknown command: enter \"help\" to list available commands."),
         }
+        println!();
     }
 }
